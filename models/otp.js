4ce665486d8d3530a1ad4a8ccfe35805cd930628
PatrickEinstein <mohammedola1234@gmail.com>
2023-05-25 05:33:02 -0400

    long otp3

--- models/otp.js
+++ models/otp.js
@@ -1,15 +1,35 @@
+// import mongoose from "mongoose";
+
+// const OTPschema = new mongoose.Schema(
+//   {
+//     otp: {
+//       type: Number,
+//       //   required: [true, "must enter a valid otp"],
+//     },
+//     createdAt: {
+//       type: Date,
+//       default: Date.now(),
+//       expires: 9000000000, // Set the expiration time in milliseconds
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// const OTP = mongoose.model("otp", OTPschema);
+// export default OTP;
+
 import mongoose from "mongoose";
 
 const OTPschema = new mongoose.Schema(
   {
     otp: {
       type: Number,
-      //   required: [true, "must enter a valid otp"],
+      // required: [true, "must enter a valid otp"],
     },
     createdAt: {
       type: Date,
       default: Date.now(),
-      expires: 9000000000, // Set the expiration time in milliseconds
+      expires: 600000, // Set the expiration time to 10 minutes (600,000 milliseconds)
     },
   },
   { timestamps: true }
