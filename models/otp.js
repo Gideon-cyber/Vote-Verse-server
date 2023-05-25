39813d803c9af7864dc6ad6013f56251ec2fcb65
PatrickEinstein <mohammedola1234@gmail.com>
2023-05-25 05:44:06 -0400

    otp 6

--- models/otp.js
+++ models/otp.js
@@ -24,13 +24,12 @@ const OTPschema = new mongoose.Schema(
   {
     otp: {
       type: Number,
-      // required: [true, "must enter a valid otp"],
     },
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-      expires: 600000, // Set the expiration time to 10 minutes (600,000 milliseconds)
-    },
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now(),
+    //   expires: 600000, // Set the expiration time to 10 minutes (600,000 milliseconds)
+    // },
   },
   { timestamps: true }
 );
