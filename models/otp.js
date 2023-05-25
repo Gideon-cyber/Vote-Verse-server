69a370cd726def22d6dd26c6d656551cedf0d25e
PatrickEinstein <mohammedola1234@gmail.com>
2023-05-25 04:22:39 -0400

    otp adjust

--- models/otp.js
+++ models/otp.js
@@ -9,7 +9,7 @@ const OTPschema = new mongoose.Schema(
     createdAt: {
       type: Date,
       default: Date.now(),
-      // expires: Date.now() + 90000000000,
+      expires: 90000000, // Set the expiration time in milliseconds
     },
   },
   { timestamps: true }
